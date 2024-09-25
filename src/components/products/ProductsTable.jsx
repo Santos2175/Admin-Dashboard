0f603328093a043f3c5ed6b6ca1831bee0f5dc3a
Santos2175 <santoshgurung949@gmail.com>
2024-09-25 22:09:56 +0545

    products page added

---
+++ src/components/products/ProductsTable.jsx
@@ -0,0 +1,108 @@
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import { PRODUCT_DATA } from '../../data/data.js';
+import { Edit, Search, Trash2 } from 'lucide-react';
+
+const ProductsTable = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState(PRODUCT_DATA);
+
+  const handleSearch = (e) => {
+    const term = e.target.value;
+    setSearchTerm(term);
+    const filtered = PRODUCT_DATA.filter(
+      (product) =>
+        product.name.toLocaleLowerCase().includes(term) ||
+        product.category.toLocaleLowerCase().includes(term)
+    );
+
+    setFilteredProducts(filtered);
+  };
+  return (
+    <motion.div
+      className='bg-gray-800 bg-opacity-50 backdrop-blur-md rounded-xl shadow-lg p-6 border border-gray-700 mb-8'
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.2 }}>
+      <div className='flex justify-between max-sm:flex-col-reverse items-center mb-6'>
+        <h2 className='text-xl font-semibold text-gray-100'>Product List</h2>
+        <div className='relative max-sm:mb-3'>
+          <input
+            type='text'
+            placeholder='search products...'
+            className='bg-gray-700 text-white placeholder-gray-400 rounded-lg pl-10 pr-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
+            onChange={handleSearch}
+            value={searchTerm}
+          />
+          <Search className='absolute left-3 top-2.5 text-gray-400' size={18} />
+        </div>
+      </div>
+
+      <div className='overflow-x-auto'>
+        <table className='min-w-full divide-y divide-gray-700'>
+          <thead>
+            <tr>
+              <th className='px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider'>
+                Name
+              </th>
+              <th className='px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider'>
+                Category
+              </th>
+              <th className='px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider'>
+                Price
+              </th>
+              <th className='px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider'>
+                Stock
+              </th>
+              <th className='px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider'>
+                Sales
+              </th>
+              <th className='px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider'>
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody className='divide-y divide-gray-700'>
+            {filteredProducts.map((product) => (
+              <motion.tr
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.3 }}>
+                <td className='px-6 py-4 text-sm whitespace-nowrap font-medium text-gray-100 gap-2 flex items-center'>
+                  <img
+                    src='https://images.unsplash.com/photo-1627989580309-bfaf3e58af6f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8d2lyZWxlc3MlMjBlYXJidWRzfGVufDB8fDB8fHww'
+                    alt='Product img'
+                    className='size-10 rounded-full'
+                  />
+                  {product.name}
+                </td>
+                <td className='px-6 py-4 text-sm whitespace-nowrap text-gray-300'>
+                  {product.category}
+                </td>
+                <td className='px-6 py-4 text-sm whitespace-nowrap text-gray-300'>
+                  ${product.price.toFixed(2)}
+                </td>
+                <td className='px-6 py-4 text-sm whitespace-nowrap text-gray-300'>
+                  {product.stock}
+                </td>
+                <td className='px-6 py-4 text-sm whitespace-nowrap text-gray-300'>
+                  {product.sales}
+                </td>
+                <td className='px-6 py-4 text-sm whitespace-nowrap text-gray-300'>
+                  <button className='text-indigo-400 hover:text-indigo-300 mr-2'>
+                    <Edit size={18} />
+                  </button>
+                  <button className='text-red-400 hover:text-red-300'>
+                    <Trash2 size={18} />
+                  </button>
+                </td>
+              </motion.tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </motion.div>
+  );
+};
+
+export default ProductsTable;
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,7 +1,62 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
+import Header from '../components/common/Header.jsx';
+import StatCard from '../components/common/StatCard.jsx';
+
+import { Package, TrendingUp, DollarSign, AlertTriangle } from 'lucide-react';
+import ProductsTable from '../components/products/ProductsTable.jsx';
+import CategoryDistributionChart from '../components/overview/CategoryDistributionChart.jsx';
+import SalesTrendChart from '../components/products/SalesTrendChart.jsx';
 
 const ProductsPage = () => {
-  return <div>products</div>;
+  return (
+    <div className='flex-1 overflow-auto relative z-10'>
+      <Header title='Products' />
+
+      <main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
+        {/* STATS */}
+        <motion.div
+          className='grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mb-8'
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}>
+          <StatCard
+            name='Total Products'
+            icon={Package}
+            value={1265}
+            color='#6366F1'
+          />
+          <StatCard
+            name='Top Selling'
+            icon={TrendingUp}
+            value={89}
+            color='#10B981'
+          />
+          <StatCard
+            name='Low Stock'
+            icon={AlertTriangle}
+            value={24}
+            color='#F59E0B'
+          />
+          <StatCard
+            name='Total Revenue'
+            icon={DollarSign}
+            value={'$543,210'}
+            color='#EF4444'
+          />
+        </motion.div>
+
+        <ProductsTable />
+
+        {/* CHARTS */}
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
+          <SalesTrendChart />
+          <CategoryDistributionChart />
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default ProductsPage;
