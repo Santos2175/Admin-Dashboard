c8df9ed9e83fc2abf436e3bb6f4d3f8734fe90e4
Santos2175 <santoshgurung949@gmail.com>
2024-10-10 15:11:03 +0545

    security section added in SettingPage

--- src/components/settings/Security.jsx
+++ src/components/settings/Security.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import { useState } from 'react';
+import SettingSection from './SettingSection.jsx';
+import ToggleSwitch from './ToggleSwitch.jsx';
+import { Lock } from 'lucide-react';
 
 const Security = () => {
-  return <div></div>;
+  const [twoFactor, setTwoFactor] = useState(false);
+  return (
+    <SettingSection icon={Lock} title={'Security'}>
+      <ToggleSwitch
+        label={'Two-Factor Authentication'}
+        isOn={twoFactor}
+        onToggle={() => setTwoFactor(!twoFactor)}
+      />
+      <div className='mt-4'>
+        <button className='bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 rounded font-bold  transition duration-200'>
+          Change Password
+        </button>
+      </div>
+    </SettingSection>
+  );
 };
 
 export default Security;
